e69e347e4eb08aa0424f9a2c143a9f9b2ca87664
machakux <bmachaku@gmail.com>
2015-02-10 08:44:21 +0300

    remove download url

--- setup.py
+++ setup.py
@@ -20,7 +20,6 @@ setup(
     author='Emil',
     author_email='emil@tehamalab.com',
     url='https://pypi.python.org/pypi/giokoda',
-    download_url='https://github.com/WorldBank-Transport/giokoda/tarball/%s' %version,
     install_requires = ["geopy"],
     scripts=['geocode_csv'],
     license = 'Apache License, Version 2.0',
